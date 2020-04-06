7d6544246c7ce1288b94ed2641e1c0ed6c4a64e8
Petch <petchkubb@live.com>
2020-04-06 13:50:15 +0700

    setup react navigation

---
+++ src/screens/indexScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const indexScreen = () => {
+  return (
+    <View>
+      <Text>index Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default indexScreen;
